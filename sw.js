a352baee6288ea74d44ae1546490448284baef7f
arthurwalter261-prog <arthurwalter261@gmail.com>
2026-04-10 19:13:19 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'planner-cache-v18';
+const CACHE_NAME = 'planner-cache-v19';
 const urlsToCache = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', event => {
